43aaf0bc894381da8810c5d05ec7d79af804be91
katharinakatharina <katharina.gottwald@gmx.de>
2018-04-24 19:13:00 +0200

    3.9 Fixed Indentation issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   get 'simple_pages/products'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/about'
-get 'simple_pages/contact'
-get 'simple_pages/index'
-get 'simple_pages/products'
-get 'simple_pages/section1'
-get 'simple_pages/section2'
-get 'simple_pages/section3'
+  get 'simple_pages/index'
+  get 'simple_pages/section1'
+  get 'simple_pages/section2'
+  get 'simple_pages/section3'
   root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
