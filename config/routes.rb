2319b9f50d5dadefda7fe758ce30eb9b135998cb
katharinakatharina <katharina.gottwald@gmx.de>
2018-04-28 19:59:25 +0200

    4.2 Adding controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'layouts/header'
   get 'simple_pages/products'
   get 'simple_pages/about'
@@ -8,6 +9,6 @@ Rails.application.routes.draw do
   get 'simple_pages/section1'
   get 'simple_pages/section2'
   get 'simple_pages/section3'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
