1f6b0117625284a128acb0caf21e5e7d33b47a49
katharinakatharina <katharina.gottwald@gmx.de>
2018-04-28 20:24:14 +0200

    4.2 Updating orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,36 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.all
   end
 
   def show
   end
 
   def new
+    @order = Order.new
   end
 
   def create
   end
 
   def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
+#  private
+#    # Use callbacks to share common setup or constraints between actions.
+#    def set_order
+#      @order = Order.find(params[:id])
+#    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+#    def order_params
+#      params.require(:product).permit(:name, :description, :image_url, :price, :color, :brand, :brand_image, :article_number)
+#    end
 
 end
