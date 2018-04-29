88d59d0455a9b79d0477d3d9a82c3564a4062ae2
katharinakatharina <katharina.gottwald@gmx.de>
2018-04-29 19:58:38 +0200

    4.5 Views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,7 +1,12 @@
 class SimplePagesController < ApplicationController
 
   def landing_page
-    @featured_product = Product.first
+    @products = Product.all
+  end
+
+  #Display random product
+  def Product.random
+    self.limit(1).offset(rand(self.count)).first
   end
 
 end
