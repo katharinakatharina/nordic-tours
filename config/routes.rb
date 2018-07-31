d79c68f383213a94766edb72ceb26b1c8120ddfb
katharinakatharina <katharina.gottwald@gmx.de>
2018-07-31 10:01:01 +0200

    5.8 Comments Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :users, only: [:new, :create, :edit, :update, :destroy]
   get 'layouts/header'
