ce421fb5831af252f982cd43674ea368140b153c
katharinakatharina <katharina.gottwald@gmx.de>
2018-08-01 11:00:40 +0200

    5.5 Authorization

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ApplicationMailer
          to: 'katharina.gottwald@gmx.de',
          subject: "A new contact form message from #{name}")
   end
+  def welcome(user)
+    @appname = "Bike Shop"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
+  end
 end
